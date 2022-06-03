b81efcaa34e4b2b81b3b1f145a30f409eb479f39
USER <USER@192.168.28.204>
2022-06-03 18:34:30 +0530

    first commit

---
+++ project1/src/project1/Testdemo.java
@@ -0,0 +1,15 @@
+package project1;
+
+public class Testdemo {
+	int x; 
+
+}
+class testdemo 
+{
+public static void main (String[]arg) 
+{
+	Testdemo myObj = new Testdemo ();
+	myObj.x= 40;
+	System.out.println(myObj.x);
+}
+}
---
+++ project1/src/project1/demo.java
@@ -0,0 +1,15 @@
+package project1;
+
+public class demo {
+	int x; 
+
+public static void main (String[]arg) 
+{
+	demo myObj1=new demo ();
+	demo myObj2=new demo ();
+	myObj1.x=24;
+	myObj2.x=55;
+	System.out.println(myObj1.x);
+	System.out.println(myObj2.x);
+}
+}
